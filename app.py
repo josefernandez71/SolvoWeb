466b4099150a7dffbdef79864d0df193d0f6fa16
Edwin Mauricio Toro Alvarez <edwin_toro23191@elpoli.edu.co>
2022-06-09 13:23:05 -0500

    Formato para trabajar con flask

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app=Flask(__name__)
 app.config['MYSQL_HOST']='localhost'
 app.config['MYSQL_USER']='root'
 app.config['MYSQL_PASSWORD']=''
-app.config['MYSQL_DB']='solvoweb'
+app.config['MYSQL_DB']='solvo'
 mysql=MySQL(app)
 
 # settings
@@ -16,9 +16,27 @@ app.secret_key='mysecretkey'
 @app.route('/')
 def index():
     return render_template('proto_solvo.html')
-@app.route('/menu')
+@app.route('/menu',methods=['POST'])
 def menu():
-    return render_template('menu.html')
+    data=none
+    if request.method=='POST':
+        
+        usuario=request.form['user']
+        password=request.form['pass']
+        if usuario !="" and password != "":
+            cur=mysql.connection.cursor()        
+            cur.execute('select * from usuarios where name=%s and passwd=%s',(usuario,password))
+            data=cur.fetchall()           
+            try:
+                if data!=none:
+                    return render_template('menu.html', contact=data[0])
+            except Exception as e:
+                print ("No existe el usuario",sys.exc_info()[0])
+                return redirect(url_for('index'))
+        else:
+            return redirect(url_for('index'))
+    else:
+        return render_template('proto_solvo.html')
 
 if __name__ == '__main__':
     app.run(port=3000,debug=True)
